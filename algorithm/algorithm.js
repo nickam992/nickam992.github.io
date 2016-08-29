f90ec339778ea9989ea4a8ec574d93d0084b09aa
nickam992 <nickam992@gmail.com>
2016-08-29 14:46:01 -0700

    added features to monte carlo

--- algorithm/algorithm.js
+++ algorithm/algorithm.js
@@ -1,15 +1,16 @@
 
 $("#bet").click(bet);
+$("#bet1000").click(bet1000);
 
 function bet() {
 	var rand = Math.random();
 	var startingMoney = parseInt($("#startingMoney").html())
-	var highestMoney = parseInt($("#highestMoney").html());
 	var numWagers = parseInt($("#numWagers").html());
 	var currentMoney = parseInt($("#currentMoney").html());
 	var currentWager = parseInt($("#currentWager").html());
 
 	if (rand >.5) {
+		var newMoney = currentMoney + currentWager;
 		$("#currentMoney").html(currentMoney + currentWager);
 		$("#currentWager").html(1);
 		$("#previousWager").html("Won");
@@ -20,4 +21,18 @@ function bet() {
 		$("#previousWager").html("Lost");
 	}
 	$("#numWagers").html(numWagers+1);
+	if(parseInt($("#currentMoney").html()) <= 0){
+		$("#previousWager").html("BANKRUPT");
+		$("#bet").attr("disabled", "disabled");
+		$("#bet1000").attr("disabled", "disabled");
+	}
+}
+
+function bet1000() {
+	for (var i=0; i<1000; i++) {
+		bet();
+		if(parseInt($("#currentMoney").html()) <= 0){
+			break;
+		}
+	}
 }
