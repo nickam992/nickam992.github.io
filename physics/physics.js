7c6d18e7f706100813f589a5e532e68cbd92f6ca
nickam992 <nickam992@gmail.com>
2017-07-05 15:28:49 -0700

    physics stuff

--- physics/physics.js
+++ physics/physics.js
@@ -1,1 +1,26 @@
-console.log($("#hello"));
+
+var counter = 100;
+setInitialConditions();
+setInterval(mainLoop, 50);
+
+function setInitialConditions() {
+	$("#graviton").css("left", counter);
+	$("#graviton").css("visibility", "visible");
+}
+
+function mainLoop(){
+	counter++;
+	$("#graviton").css("left", counter);
+}
+
+
+function getSatelitePosition(){
+  h = counter;
+
+  //(x – h)2 + (y – k)2 = r2
+}
+
+
+
+
+$("#graviton").style
